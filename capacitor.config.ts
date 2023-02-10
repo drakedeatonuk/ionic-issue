b06de2be38d0b9741e774b01ef6699919c88e8be
Drake <drake.s.deaton@gmail.com>
2023-02-10 15:53:33 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appName: 'app',
+  webDir: 'dist/apps/app/www',
+  bundledWebRuntime: false,
+};
+
+export default config;
